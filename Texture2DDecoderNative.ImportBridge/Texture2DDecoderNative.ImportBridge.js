c93ce000cd2f2164102fee5f2dee2a609e28999a
Wad <wad8989@dev>
2025-10-11 22:49:32 +0100

    [Texture2DDecoderNative.WebAdapter] Fix crash
    [Texture2DDecoderNative.WebAdapter] Fix UnpackCrunchXXX
    
    [Texture2DDecoderNative.WebAdapter] Decode Texture2D

--- Texture2DDecoderNative.ImportBridge/Texture2DDecoderNative.ImportBridge.js
+++ Texture2DDecoderNative.ImportBridge/Texture2DDecoderNative.ImportBridge.js
@@ -2,33 +2,23 @@ import createModule from './Texture2DDecoderNative.WebAdapter.js';
 
 const Module = await createModule();
 
-export const { 
-    _DecodeDXT1,
-    _DecodeDXT5,
-    _DecodePVRTC,
-    _DecodeETC1,
-    _DecodeETC2,
-    _DecodeETC2A1,
-    _DecodeETC2A8,
-    _DecodeEACR,
-    _DecodeEACRSigned,
-    _DecodeEACRG,
-    _DecodeEACRGSigned,
-    _DecodeBC4,
-    _DecodeBC5,
-    _DecodeBC6,
-    _DecodeBC7,
-    _DecodeATCRGB4,
-    _DecodeATCRGBA8,
-    _DecodeASTC,
-    _UnpackCrunch,
-    _UnpackUnityCrunch,
-    _DisposeBuffer,
-    _malloc,
-    _free,
-    __emscripten_stack_restore,
-    __emscripten_stack_alloc,
-    _emscripten_stack_get_current,
-    HEAPU8,
-    HEAP32
+export const {
+  _DecodeDXT1, _DecodeDXT5, _DecodePVRTC, _DecodeETC1, _DecodeETC2,
+  _DecodeETC2A1, _DecodeETC2A8, _DecodeEACR, _DecodeEACRSigned,
+  _DecodeEACRG, _DecodeEACRGSigned, _DecodeBC4, _DecodeBC5,
+  _DecodeBC6, _DecodeBC7, _DecodeATCRGB4, _DecodeATCRGBA8,
+  _DecodeASTC, _UnpackCrunch, _UnpackUnityCrunch, _DisposeBuffer,
+  _malloc, _free
 } = Module;
+
+
+export function _HEAP_WriteData(heapAddr, srcData)
+{
+  Module.HEAPU8.set(srcData, heapAddr);
+}
+
+export function _HEAP_ReadData(heapAddr, len)
+{
+  console.log("_HEAP_ReadData", heapAddr, len);
+  return Module.HEAPU8.slice(heapAddr, heapAddr + len);
+}
--- Texture2DDecoderNative.ImportBridge/TextureDecoder.cs
+++ Texture2DDecoderNative.ImportBridge/TextureDecoder.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Linq;
 using System.Runtime.InteropServices.JavaScript;
+using System.Runtime.InteropServices;
 
 namespace Texture2DDecoder
 {
@@ -64,10 +66,10 @@ namespace Texture2DDecoder
         private static partial int DecodeASTCNative(IntPtr data, int width, int height, int blockWidth, int blockHeight, IntPtr output);
 
         [JSImport("_UnpackCrunch", ModuleName)]
-        private static partial IntPtr UnpackCrunchNative(IntPtr data, int dataSize, IntPtr outputSize);
+        private static partial void UnpackCrunchNative(IntPtr data, int dataSize, IntPtr out_ppResult, IntPtr out_pSize);
 
         [JSImport("_UnpackUnityCrunch", ModuleName)]
-        private static partial IntPtr UnpackUnityCrunchNative(IntPtr data, int dataSize, IntPtr outputSize);
+        private static partial void UnpackUnityCrunchNative(IntPtr data, int dataSize, IntPtr out_ppResult, IntPtr out_pSize);
 
         [JSImport("_DisposeBuffer", ModuleName)]
         private static partial void DisposeBufferNative(IntPtr buffer);
@@ -78,283 +80,203 @@ namespace Texture2DDecoder
         [JSImport("_free", ModuleName)]
         private static partial void FreeNative(IntPtr ptr);
 
+        [JSImport("_HEAP_ReadData", ModuleName)]
+        private static partial byte[] HEAP_ReadDataNative(IntPtr heapAddr, IntPtr len);
+
+        [JSImport("_HEAP_WriteData", ModuleName)]
+        private static partial void HEAP_WriteDataNative(IntPtr heapAddr, byte[] srcData);
+
         // Public API - matches original Texture2DDecoder interface
         public static bool DecodeDXT1(ReadOnlySpan<byte> data, int width, int height, Span<byte> output)
         {
-            unsafe
-            {
-                fixed (byte* dataPtr = data)
-                fixed (byte* outputPtr = output)
-                {
-                    return DecodeDXT1Native((IntPtr)dataPtr, width, height, (IntPtr)outputPtr) != 0;
-                }
-            }
+            return PrepareDecodeWithHeap(data, output, (wasm_dataPtr, wasm_outputPtr) => DecodeDXT1Native(wasm_dataPtr, width, height, wasm_outputPtr) != 0);
         }
 
         public static bool DecodeDXT5(ReadOnlySpan<byte> data, int width, int height, Span<byte> output)
         {
-            unsafe
-            {
-                fixed (byte* dataPtr = data)
-                fixed (byte* outputPtr = output)
-                {
-                    return DecodeDXT5Native((IntPtr)dataPtr, width, height, (IntPtr)outputPtr) != 0;
-                }
-            }
+            return PrepareDecodeWithHeap(data, output, (wasm_dataPtr, wasm_outputPtr) => DecodeDXT5Native(wasm_dataPtr, width, height, wasm_outputPtr) != 0);
         }
 
         public static bool DecodePVRTC(ReadOnlySpan<byte> data, int width, int height, Span<byte> output, bool is2bpp)
         {
-            unsafe
-            {
-                fixed (byte* dataPtr = data)
-                fixed (byte* outputPtr = output)
-                {
-                    return DecodePVRTCNative((IntPtr)dataPtr, width, height, (IntPtr)outputPtr, is2bpp ? 1 : 0) != 0;
-                }
-            }
+            return PrepareDecodeWithHeap(data, output, (wasm_dataPtr, wasm_outputPtr) => DecodePVRTCNative(wasm_dataPtr, width, height, wasm_outputPtr, is2bpp ? 1 : 0) != 0);
         }
 
         public static bool DecodeETC1(ReadOnlySpan<byte> data, int width, int height, Span<byte> output)
         {
-            unsafe
-            {
-                fixed (byte* dataPtr = data)
-                fixed (byte* outputPtr = output)
-                {
-                    return DecodeETC1Native((IntPtr)dataPtr, width, height, (IntPtr)outputPtr) != 0;
-                }
-            }
+            return PrepareDecodeWithHeap(data, output, (wasm_dataPtr, wasm_outputPtr) => DecodeETC1Native(wasm_dataPtr, width, height, wasm_outputPtr) != 0);
         }
 
         public static bool DecodeETC2(ReadOnlySpan<byte> data, int width, int height, Span<byte> output)
         {
-            unsafe
-            {
-                fixed (byte* dataPtr = data)
-                fixed (byte* outputPtr = output)
-                {
-                    return DecodeETC2Native((IntPtr)dataPtr, width, height, (IntPtr)outputPtr) != 0;
-                }
-            }
+            return PrepareDecodeWithHeap(data, output, (wasm_dataPtr, wasm_outputPtr) => DecodeETC2Native(wasm_dataPtr, width, height, wasm_outputPtr) != 0);
         }
 
         public static bool DecodeETC2A1(ReadOnlySpan<byte> data, int width, int height, Span<byte> output)
         {
-            unsafe
-            {
-                fixed (byte* dataPtr = data)
-                fixed (byte* outputPtr = output)
-                {
-                    return DecodeETC2A1Native((IntPtr)dataPtr, width, height, (IntPtr)outputPtr) != 0;
-                }
-            }
+            return PrepareDecodeWithHeap(data, output, (wasm_dataPtr, wasm_outputPtr) => DecodeETC2A1Native(wasm_dataPtr, width, height, wasm_outputPtr) != 0);
         }
 
         public static bool DecodeETC2A8(ReadOnlySpan<byte> data, int width, int height, Span<byte> output)
         {
-            unsafe
-            {
-                fixed (byte* dataPtr = data)
-                fixed (byte* outputPtr = output)
-                {
-                    return DecodeETC2A8Native((IntPtr)dataPtr, width, height, (IntPtr)outputPtr) != 0;
-                }
-            }
+            return PrepareDecodeWithHeap(data, output, (wasm_dataPtr, wasm_outputPtr) => DecodeETC2A8Native(wasm_dataPtr, width, height, wasm_outputPtr) != 0);
         }
 
         public static bool DecodeEACR(ReadOnlySpan<byte> data, int width, int height, Span<byte> output)
         {
-            unsafe
-            {
-                fixed (byte* dataPtr = data)
-                fixed (byte* outputPtr = output)
-                {
-                    return DecodeEACRNative((IntPtr)dataPtr, width, height, (IntPtr)outputPtr) != 0;
-                }
-            }
+            return PrepareDecodeWithHeap(data, output, (wasm_dataPtr, wasm_outputPtr) => DecodeEACRNative(wasm_dataPtr, width, height, wasm_outputPtr) != 0);
         }
 
         public static bool DecodeEACRSigned(ReadOnlySpan<byte> data, int width, int height, Span<byte> output)
         {
-            unsafe
-            {
-                fixed (byte* dataPtr = data)
-                fixed (byte* outputPtr = output)
-                {
-                    return DecodeEACRSignedNative((IntPtr)dataPtr, width, height, (IntPtr)outputPtr) != 0;
-                }
-            }
+            return PrepareDecodeWithHeap(data, output, (wasm_dataPtr, wasm_outputPtr) => DecodeEACRSignedNative(wasm_dataPtr, width, height, wasm_outputPtr) != 0);
         }
 
         public static bool DecodeEACRG(ReadOnlySpan<byte> data, int width, int height, Span<byte> output)
         {
-            unsafe
-            {
-                fixed (byte* dataPtr = data)
-                fixed (byte* outputPtr = output)
-                {
-                    return DecodeEACRGNative((IntPtr)dataPtr, width, height, (IntPtr)outputPtr) != 0;
-                }
-            }
+            return PrepareDecodeWithHeap(data, output, (wasm_dataPtr, wasm_outputPtr) => DecodeEACRGNative(wasm_dataPtr, width, height, wasm_outputPtr) != 0);
         }
 
         public static bool DecodeEACRGSigned(ReadOnlySpan<byte> data, int width, int height, Span<byte> output)
         {
-            unsafe
-            {
-                fixed (byte* dataPtr = data)
-                fixed (byte* outputPtr = output)
-                {
-                    return DecodeEACRGSignedNative((IntPtr)dataPtr, width, height, (IntPtr)outputPtr) != 0;
-                }
-            }
+            return PrepareDecodeWithHeap(data, output, (wasm_dataPtr, wasm_outputPtr) => DecodeEACRGSignedNative(wasm_dataPtr, width, height, wasm_outputPtr) != 0);
         }
 
         public static bool DecodeBC4(ReadOnlySpan<byte> data, int width, int height, Span<byte> output)
         {
-            unsafe
-            {
-                fixed (byte* dataPtr = data)
-                fixed (byte* outputPtr = output)
-                {
-                    return DecodeBC4Native((IntPtr)dataPtr, width, height, (IntPtr)outputPtr) != 0;
-                }
-            }
+            return PrepareDecodeWithHeap(data, output, (wasm_dataPtr, wasm_outputPtr) => DecodeBC4Native(wasm_dataPtr, width, height, wasm_outputPtr) != 0);
         }
 
         public static bool DecodeBC5(ReadOnlySpan<byte> data, int width, int height, Span<byte> output)
         {
-            unsafe
-            {
-                fixed (byte* dataPtr = data)
-                fixed (byte* outputPtr = output)
-                {
-                    return DecodeBC5Native((IntPtr)dataPtr, width, height, (IntPtr)outputPtr) != 0;
-                }
-            }
+            return PrepareDecodeWithHeap(data, output, (wasm_dataPtr, wasm_outputPtr) => DecodeBC5Native(wasm_dataPtr, width, height, wasm_outputPtr) != 0);
         }
 
         public static bool DecodeBC6(ReadOnlySpan<byte> data, int width, int height, Span<byte> output)
         {
-            unsafe
-            {
-                fixed (byte* dataPtr = data)
-                fixed (byte* outputPtr = output)
-                {
-                    return DecodeBC6Native((IntPtr)dataPtr, width, height, (IntPtr)outputPtr) != 0;
-                }
-            }
+            return PrepareDecodeWithHeap(data, output, (wasm_dataPtr, wasm_outputPtr) => DecodeBC6Native(wasm_dataPtr, width, height, wasm_outputPtr) != 0);
         }
 
         public static bool DecodeBC7(ReadOnlySpan<byte> data, int width, int height, Span<byte> output)
         {
-            unsafe
-            {
-                fixed (byte* dataPtr = data)
-                fixed (byte* outputPtr = output)
-                {
-                    return DecodeBC7Native((IntPtr)dataPtr, width, height, (IntPtr)outputPtr) != 0;
-                }
-            }
+            return PrepareDecodeWithHeap(data, output, (wasm_dataPtr, wasm_outputPtr) => DecodeBC7Native(wasm_dataPtr, width, height, wasm_outputPtr) != 0);
         }
 
         public static bool DecodeATCRGB4(ReadOnlySpan<byte> data, int width, int height, Span<byte> output)
         {
-            unsafe
-            {
-                fixed (byte* dataPtr = data)
-                fixed (byte* outputPtr = output)
-                {
-                    return DecodeATCRGB4Native((IntPtr)dataPtr, width, height, (IntPtr)outputPtr) != 0;
-                }
-            }
+            return PrepareDecodeWithHeap(data, output, (wasm_dataPtr, wasm_outputPtr) => DecodeATCRGB4Native(wasm_dataPtr, width, height, wasm_outputPtr) != 0);
         }
 
         public static bool DecodeATCRGBA8(ReadOnlySpan<byte> data, int width, int height, Span<byte> output)
         {
-            unsafe
-            {
-                fixed (byte* dataPtr = data)
-                fixed (byte* outputPtr = output)
-                {
-                    return DecodeATCRGBA8Native((IntPtr)dataPtr, width, height, (IntPtr)outputPtr) != 0;
-                }
-            }
+            return PrepareDecodeWithHeap(data, output, (wasm_dataPtr, wasm_outputPtr) => DecodeATCRGBA8Native(wasm_dataPtr, width, height, wasm_outputPtr) != 0);
         }
 
         public static bool DecodeASTC(ReadOnlySpan<byte> data, int width, int height, int blockWidth, int blockHeight, Span<byte> output)
         {
-            unsafe
-            {
-                fixed (byte* dataPtr = data)
-                fixed (byte* outputPtr = output)
-                {
-                    return DecodeASTCNative((IntPtr)dataPtr, width, height, blockWidth, blockHeight, (IntPtr)outputPtr) != 0;
-                }
-            }
+            return PrepareDecodeWithHeap(data, output, (wasm_dataPtr, wasm_outputPtr) => DecodeASTCNative(wasm_dataPtr, width, height, blockWidth, blockHeight, wasm_outputPtr) != 0);
         }
+
+        private static bool PrepareDecodeWithHeap(ReadOnlySpan<byte> data, Span<byte> output, Func<IntPtr, IntPtr, bool> decodeAction)
+    {
+        IntPtr dataPtr = IntPtr.Zero;
+        IntPtr outputPtr = IntPtr.Zero;
+        try
+        {
+            dataPtr = MallocNative(data.Length);
+            outputPtr = MallocNative(output.Length);
+
+            if (dataPtr == IntPtr.Zero || outputPtr == IntPtr.Zero)
+            {
+                // Memory allocation failed
+                return false;
+            }
+
+            // Write input data to the allocated heap memory
+            HEAP_WriteDataNative(dataPtr, data.ToArray());
+
+            // Execute the specific native decode function
+            bool success = decodeAction(dataPtr, outputPtr);
+
+            if (success)
+            {
+                // If successful, read the result back from the heap
+                byte[] resultData = HEAP_ReadDataNative(outputPtr, (IntPtr)output.Length);
+                resultData.CopyTo(output);
+            }
+
+            return success;
+        }
+        finally
+        {
+            // Always free the allocated memory to prevent leaks
+            if (dataPtr != IntPtr.Zero) FreeNative(dataPtr);
+            if (outputPtr != IntPtr.Zero) FreeNative(outputPtr);
+        }
+    }
 
         public static byte[] UnpackCrunch(ReadOnlySpan<byte> data)
         {
-            return UnpackCrunchInternal(data, false).ToArray();
+            return UnpackCrunchInternal(data, false);
         }
 
         public static byte[] UnpackUnityCrunch(ReadOnlySpan<byte> data)
         {
-            return UnpackCrunchInternal(data, true).ToArray();
+            return UnpackCrunchInternal(data, true);
         }
 
-        private static ReadOnlySpan<byte> UnpackCrunchInternal(ReadOnlySpan<byte> data, bool isUnityCrunch)
+        private static byte[] UnpackCrunchInternal(ReadOnlySpan<byte> data, bool isUnityCrunch)
         {
             unsafe
             {
-                fixed (byte* dataPtr = data)
                 {
-                    // Allocate space for output size
-                    IntPtr outputSizePtr = MallocNative(4);
-                    
+                    IntPtr in_wasm_dataPtr = IntPtr.Zero;
+                    IntPtr out_wasm_sizePtr = IntPtr.Zero;
+                    IntPtr out_wasm_resultPtrPtr = IntPtr.Zero;
                     try
                     {
-                        IntPtr resultPtr;
+                        in_wasm_dataPtr = MallocNative(data.Length);
+                        out_wasm_sizePtr = MallocNative(4); // c++ sizeof(uint32_t)
+                        out_wasm_resultPtrPtr = MallocNative(8); // c++ sizeof(void*)
+
+                        HEAP_WriteDataNative(in_wasm_dataPtr, data.ToArray());
+
                         if (isUnityCrunch)
                         {
-                            resultPtr = UnpackUnityCrunchNative((IntPtr)dataPtr, data.Length, outputSizePtr);
+                            UnpackUnityCrunchNative(in_wasm_dataPtr, data.Length, out_wasm_resultPtrPtr, out_wasm_sizePtr);
                         }
                         else
                         {
-                            resultPtr = UnpackCrunchNative((IntPtr)dataPtr, data.Length, outputSizePtr);
+                            UnpackCrunchNative(in_wasm_dataPtr, data.Length, out_wasm_resultPtrPtr, out_wasm_sizePtr);
                         }
-
-                        if (resultPtr == IntPtr.Zero)
-                        {
-                            return ReadOnlySpan<byte>.Empty;
-                        }
-
-                        // Read the output size
-                        int outputSize = *(int*)outputSizePtr;
                         
-                        if (outputSize <= 0)
-                        {
-                            DisposeBufferNative(resultPtr);
-                            return ReadOnlySpan<byte>.Empty;
-                        }
+                        // Read the output size
+                        uint resultSize = BitConverter.ToUInt32(HEAP_ReadDataNative(out_wasm_sizePtr, 4));
+                        IntPtr resultPtr = new IntPtr(BitConverter.ToInt64(HEAP_ReadDataNative(out_wasm_resultPtrPtr, 8)));
 
-                        // Copy data to managed array
-                        byte[] result = new byte[outputSize];
-                        fixed (byte* resultArrayPtr = result)
+                        if (resultSize == 0 || resultPtr == IntPtr.Zero)
                         {
-                            Buffer.MemoryCopy((void*)resultPtr, resultArrayPtr, outputSize, outputSize);
+                            return null;
                         }
+                        
+                        // Copy data to managed array
+                        byte[] result = HEAP_ReadDataNative(resultPtr, (nint)resultSize);
 
                         // Free WASM-allocated buffer
-                        DisposeBufferNative(resultPtr);
+                        DisposeBufferNative(out_wasm_resultPtrPtr);
 
                         return result;
                     }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"UnpackCrunchInternal {ex.ToString()}");
+                        return null;
+                    }
                     finally
                     {
-                        FreeNative(outputSizePtr);
+                        FreeNative(in_wasm_dataPtr);
+                        FreeNative(out_wasm_sizePtr);
+                        FreeNative(out_wasm_resultPtrPtr);
                     }
                 }
             }
